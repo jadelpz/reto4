d357807ed3b46e65d977c259797f35cf0ee1370f
Alaa <abouzerraralaa05@gmail.com>
2024-11-19 11:48:24 +0100

    funciones1 primer commit

--- reto4/src/reto4/Funciones1.java
+++ reto4/src/reto4/Funciones1.java
@@ -1,5 +1,16 @@
 package reto4;
 
 public class Funciones1 {
-
+public static void primo(int num) {
+	int cont=1;
+	for(int i=2;i<num;i++) {
+		if(num%i==0) {
+			cont++;
+		}
+		if(cont>0) {
+			System.out.println(num+" es primo");
+		}
+			}
+		
+}
 }
