793010da9a59158dd2e7908ead7b7e39c8f8f5dd
Alaa <abouzerraralaa05@gmail.com>
2024-11-19 11:37:29 +0100

    commit 1 Alaa

---
+++ reto4/src/reto4/Funciones1.java
@@ -0,0 +1,5 @@
+package reto4;
+
+public class Funciones1 {
+
+}
---
+++ reto4/src/reto4/Reto4.java
@@ -0,0 +1,10 @@
+package reto4;
+
+public class Reto4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
